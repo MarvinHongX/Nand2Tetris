71d38ff33d7b5e9aa737b666cb9ec028c2c9dd92
Marvin Hong <marvinhongx@gmail.com>
2023-07-11 15:11:44 +0900

    first

---
+++ 04/fill/Fill.asm
@@ -0,0 +1,75 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+(INIT)
+    @32
+    D=A
+    @col
+    M=D       // col=32 (16bit x 32 = 512)
+    @256
+    D=A
+    @row
+    M=D       // row=256
+
+    @SCREEN
+    D=A
+    @addr
+    M=D           // addr=16384
+    @i
+    M=0           // i=0
+
+(LOOP)
+    @j
+    M=0           // j=0
+    @i
+    D=M           // D=i
+    @row
+    D=D-M         // D=i-row
+    @INIT
+    D;JGE         // If (i-row)>=0 goto (LOOP)
+(SUB_LOOP)
+    @j
+    D=M           // D=j
+    @col
+    D=D-M         // D=j-col
+    @SUB_END
+    D;JGE         // If (j-col)>=0 goto SUBEND
+    
+    @density
+    M=0           // density=0 (0000000000000000)
+    @KBD
+    D=M
+    @ONOFF
+    D;JEQ         // If (KBD value)=0 goto ONOFF 
+    @density
+    M=-1          // density=-1 (1111111111111111)
+
+(ONOFF)
+    @density
+    D=M           // D=density
+    @addr
+    A=M
+    M=D           // RAM[addr]=density
+    
+    @j
+    M=M+1         // j=j+1
+    @addr
+    M=M+1         // addr=addr+1
+
+    @SUB_LOOP
+    0;JMP         // goto SUBLOOP
+(SUB_END)
+    @i
+    M=M+1         // i=i+1
+    @LOOP
+    0;JMP         // goto LOOP
---
+++ 04/mult/Mult.asm
@@ -0,0 +1,58 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+
+	@R1
+	D=M     // D=R1
+	@R0
+	D=D-M   // D=R1-R0
+	@INIT
+	D;JGE   // if (R1-R0) >= 0, goto INIT
+
+(SWAP)
+	@R0
+	D=M     // D=R0
+	@temp
+	M=D     // temp=R0
+	@R1
+	D=M     // D=R1
+	@R0
+	M=D     // R0=D
+	@temp
+	D=M     // D=temp
+	@R1
+	M=D     // R1=D
+(INIT)
+	@temp
+	M=0     // temp=0
+	@R2
+	M=0     // R2=0
+	@i
+	M=0     // i=0
+(LOOP)
+	@i
+	D=M     // D=i
+	@R0
+	D=D-M   // D=i-R0
+	@END
+	D;JGE   // If (i-R0)>=0 goto END
+
+	@R1
+	D=M     // D=R1
+	@R2
+	M=D+M   // R2=R1+R2
+	@i
+	M=M+1   // i=i+1
+	@LOOP
+	0;JMP   // Goto LOOP
+(END)
+	@END
+	0;JMP
